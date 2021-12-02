f021984f17f2f1cb2f8163f12700eef044493ba7
elendi1 <2ti7xaoj@anonaddy.me>
2021-12-02 11:45:15 +0100

    Added colorbar patch to patch.sh

--- patch.sh
+++ patch.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-patch < patches/dwm-status2d-20200508-60bb3df.diff
-patch < patches/dwm-status2d-swap-save-restore-6.2.diff
-patch < patches/dwm-vanitygaps-20200610-f09418b.diff
-patch < patches/dwm-alwayscenter-20200625-f04cac6.diff
-patch < patches/dwm-rotatestack-20161021-ab9571b.diff
-patch < patches/dwm-warp-6.2.diff
-patch < patches/dwm-noborderfloatingfix-6.2.diff
+patch < patches/dwm-colorbar-6.2.diff
