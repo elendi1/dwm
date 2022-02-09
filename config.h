26d26d6df8a0a660c984b0307ec14400eaaba805
elendi1 <2ti7xaoj@anonaddy.me>
2022-02-09 17:59:11 +0100

    Added pertag patch. Fixed patch.sh. DejaVu is now not condensed

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //static const char *fonts[]          = {"Symbols Nerd Font:12", "monospace:size=10"};
-static const char *fonts[]          = {"DejaVu Sans Condensed:size=10", "Font Awesome 6 Free:size=11", "Font Awesome 6 Free Solid:size=11", "Font Awesome 6 Brands:size=11"};
+static const char *fonts[]          = {"DejaVu Sans:size=10", "Font Awesome 6 Free:size=11", "Font Awesome 6 Free Solid:size=11", "Font Awesome 6 Brands:size=11"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
--- patch.sh
+++ patch.sh
@@ -1,1 +1,11 @@
-patch < patches/dwm-colorbar-6.2.diff
+#!/bin/bash
+
+patch < patches/dwm-status2d-20200508-60bb3df.diff
+patch < patches/dwm-status2d-swap-save-restore-6.2.diff
+patch < patches/dwm-vanitygaps-20200610-f09418b.diff
+patch < patches/dwm-alwayscenter-20200625-f04cac6.diff
+patch < patches/dwm-rotatestack-20161021-ab9571b.diff
+patch < patches/dwm-warp-6.2.diff
+patch < patches/dwm-noborderfloatingfix-6.2.diff
+patch < patches/dwm-colorbar-6.2_mod.diff
+patch < patches/dwm-pertag-20200914-61bb8b2.diff
