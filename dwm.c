5c875dc377ff40fb449288dcc2375405e8070784
elendi1 <2ti7xaoj@anonaddy.me>
2022-02-11 12:28:26 +0100

    Rect always shown in bar at different y positions to give an indication of the number of the tag

--- dwm.c
+++ dwm.c
@@ -908,8 +908,7 @@ drawbar(Monitor *m)
 		tagw[i] = w = TEXTW(masterclientontag[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeTagsSel : SchemeTagsNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, masterclientontag[i], urg & 1 << i);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
+		drw_rect(drw, x + boxs, ((i % 3) + 1) * boxs + (i % 3) * drw->fonts->h/3, boxw, boxw,
 				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 				urg & 1 << i);
 		x += w;
